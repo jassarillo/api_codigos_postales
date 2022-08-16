2494eab4b2165b63faf72f28d265309669ed529d
julio cesar arrieta santiago <jarrieta@finanzas.cdmx.gob.mx>
2022-08-16 14:44:32 -0500

    convert to int values

--- app/Http/Controllers/API/CodigosController.php
+++ app/Http/Controllers/API/CodigosController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Codigos;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CodigosController extends Controller
 {
@@ -15,20 +16,73 @@ class CodigosController extends Controller
      */
     public function index($codigo)
     {
-        
-        $codigos_result = Codigos::where('d_codigo',$codigo)->get()->toArray();
+   
 
-        $array_data = array("zip_code" => $codigo, "locality" => $codigos_result[0]['d_ciudad'], "federal_entity" 
-            => $codigos_result[0]);
-            
-        foreach ($codigos_result as $key => $value) {
-            
-            //$array_data =       $codigos_result[$key];
-            //print_r($codigos_result[$key]);
+        $header_code =  DB::table('codigos')
+            ->selectRaw('c_estado as "key" , upper(d_estado) as name, c_CP as "code"')
+            ->where('d_codigo',$codigo)
+            ->groupByRaw('c_estado, d_estado, c_CP')
+            ->get()->toArray();
+
+           
+
+        $codigos_result = Codigos::where('d_codigo',$codigo)->get()->toArray();
+        
+        //3.-
+        $settlements =  DB::table('codigos')
+            ->selectRaw('id_asenta_cpcons as "key" , upper(d_asenta) as "name",  upper(d_zona) as "zone_type", d_tipo_asenta as "type_name"')
+            ->where('d_codigo',$codigo)
+            ->get()->toArray();
+
+        $add[]=array();
+        foreach ($settlements as $key => $value) 
+        {
+                $add=array(
+                    "name" => $settlements[$key]->type_name
+                );
+                $sett[] = array(
+                            "key"   =>  $value->key * 1,
+                            "name"  =>  $value->name,
+                            "zone_type"  =>  $value->zone_type,
+                            "settlement_type"  =>  $add,
+                );     
+
         }
+
+
+        $footer_municipality =  DB::table('codigos')
+            ->selectRaw('c_mnpio as "key", upper(d_mnpio) as "name"')
+            ->where('d_codigo',$codigo)
+            ->groupByRaw('c_mnpio, d_mnpio')
+            ->get()->toArray();
+
+        foreach ($footer_municipality as $key => $value) 
+        {
+                
+                $footer_array[] = array(
+                            "key"   =>  $value->key * 1,
+                            "name"  =>  $value->name
+                           
+                           
+                );     
+
+        }
+
+        //dd($footer_array);
+        
+        $array_data = array(
+                    "zip_code"          => $codigo, 
+                    "locality"          => $codigos_result[0]['d_ciudad'], 
+                    "federal_entity"    => $header_code[0],
+                    "settlements"       => $sett,
+                    "municipality"      => $footer_array[0]
+                    );
+            
+        
 
         //dd($codigos_result);  
         return $array_data;
+        
     }
 
     /**
